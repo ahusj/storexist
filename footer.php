dbc668c5ce7fffe241f4faf7eeae91680e681ad2
ahusj <ahusjalal@gmail.com>
2017-02-04 11:08:48 +0700

    update footer widget

--- footer.php
+++ footer.php
@@ -15,9 +15,71 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo" style="background-image: url(https://btqwp.preview.decentthemes.com/wp-content/uploads/2016/09/bg-image1.png);">
 
 		<div class="container">
+
+			<div class="site-footer__top">
+
+				<div class="row">
+					
+					<div class="col-sm-6 col-md-3">
+						<section class="site-widget widget">
+							<h2 class="widget-title">Our Services</h2>
+							<div class="menu-services-container">
+								<ul class="menu-services">
+									<li class="menu-item"><a href="#">About Us</a></li>
+									<li class="menu-item"><a href="#">Order History</a></li>
+									<li class="menu-item"><a href="#">Return</a></li>
+									<li class="menu-item"><a href="#">Customer Services</a></li>
+									<li class="menu-item"><a href="#">Term & Condistions</a></li>
+								</ul>
+							</div>
+						</section>
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						<section class="site-widget widget">
+							<h2 class="widget-title">Our Services</h2>
+							<div class="menu-services-container">
+								<ul class="menu-services">
+									<li class="menu-item"><a href="#">About Us</a></li>
+									<li class="menu-item"><a href="#">Order History</a></li>
+									<li class="menu-item"><a href="#">Return</a></li>
+									<li class="menu-item"><a href="#">Customer Services</a></li>
+									<li class="menu-item"><a href="#">Term & Condistions</a></li>
+								</ul>
+							</div>
+						</section>
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						<section class="site-widget widget">
+							<h2 class="widget-title">Our Services</h2>
+							<div class="menu-services-container">
+								<ul class="menu-services">
+									<li class="menu-item"><a href="#">About Us</a></li>
+									<li class="menu-item"><a href="#">Order History</a></li>
+									<li class="menu-item"><a href="#">Return</a></li>
+									<li class="menu-item"><a href="#">Customer Services</a></li>
+									<li class="menu-item"><a href="#">Term & Condistions</a></li>
+								</ul>
+							</div>
+						</section>
+					</div>
+
+					<div class="col-sm-6 col-md-3">
+						<section class="site-widget widget">
+							<h2 class="widget-title">Our Services</h2>
+							<div class="textwidget">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur perspiciatis modi magnam, magni vero nam molestias sit in dicta recusandae accusantium id, saepe animi odio sequi ea odit qui porro.</p>
+							</div>
+						</section>
+					</div>
+
+				</div>
+
+			</div><!-- /.site-footer__top -->
 
 			<div class="site-info">
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'storexist' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'storexist' ), 'WordPress' ); ?></a>
